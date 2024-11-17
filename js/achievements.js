6616a5e6626846ff87c4cec4af3ef7b37b2bd18c
Shady-Max <kolodin2005+github@gmail.com>
2024-11-17 22:23:52 +0500

    Update achievements.js

--- js/achievements.js
+++ js/achievements.js
@@ -1,5 +1,6 @@
 async function updateColumnById() {
-    const apiUrl = 'api/api.steampowered.com-achievements.json'; // local data becouse of CORS error we can only do that on backend with node.js or something else
+    const apiUrl = 'https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=250900&format=json'; // local data becouse of CORS error we can only do that on backend with node.js or something else 
+    //   api/api.steampowered.com-achievements.json
   
     try {
       const response = await fetch(apiUrl);
