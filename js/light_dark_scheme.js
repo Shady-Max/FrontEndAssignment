e67d1b91c16ce6927d643de5fc9cc6df5465f87a
Shady-Max <kolodin2005+github@gmail.com>
2024-11-18 23:04:07 +0500

    fixed dark/light theme

--- js/light_dark_scheme.js
+++ js/light_dark_scheme.js
@@ -4,16 +4,16 @@ const body = document.body;
 let theme = localStorage.getItem('theme') || 'dark-theme';
 body.classList.add(theme);
 
-themeToggleButton.textContent = theme === 'light-theme' ? 'Switch to Dark Theme' : 'Switch to Light Theme';
+themeToggleButton.textContent = theme === 'light-theme' ? 'Dark Theme' : 'light theme';
 
 themeToggleButton.addEventListener('click', () => {
     if (body.classList.contains('light-theme')) {
         body.classList.replace('light-theme', 'dark-theme');
-        themeToggleButton.textContent = 'Switch to Light Theme';
+        themeToggleButton.textContent = 'light theme';
         localStorage.setItem('theme', 'dark-theme');
     } else {
         body.classList.replace('dark-theme', 'light-theme');
-        themeToggleButton.textContent = 'Switch to Dark Theme';
+        themeToggleButton.textContent = 'dark theme';
         localStorage.setItem('theme', 'light-theme');
     }
 });
